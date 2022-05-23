aac2d31333d12d325f94986e214bd382cface941
Masum Alam <masum.7788alam@gmail.com>
2022-05-23 16:26:52 +0530

    no change
    no change

---
+++ js/script.js
@@ -0,0 +1,71 @@
+//var array = ['getFname', ' getLname', 'getBname', 'getNber', 'getEmail', ' getAddress', 'getStreet', 'getCity', 'getState', 'getZip', 'getType', 'getReview']
+
+
+var getFname = document.getElementById('getFname').value;
+var getLname = document.getElementById('getLname').value;
+var getBname = document.getElementById('getBname').value;
+var getNber  =  document.getElementById('getNber').value;
+var getEmail = document.getElementById('getEmail').value;
+var getAddress = document.getElementById('getAddress').value;
+var getStreet = document.getElementById('getStreet').value;
+var getCity = document.getElementById('getAddress').value;
+//var getState = document.getElementById('getState').value;
+var getZip = document.getElementById('getZip').value;
+var getType = document.getElementById('getType').value;
+//var getReview = document.getElementById('getRview').value;
+
+
+function formRegister() {
+    
+    var getFname = document.getElementById('getFname').value;
+    //document.getElementById('getFname').innerText = getFname;
+    console.log(getFname);
+    
+    var getLname = document.getElementById('getLname').value;
+    //document.getElementById('getFname').innerText = getLname;
+    console.log(getLname);
+    
+    var getBname = document.getElementById('getBname').value;
+    //document.getElementById('getFname').innerText= getBname;
+    console.log(getBname);
+    
+    var getNber = document.getElementById('getNber').value;
+    //document.getElementById('getFname').innerText = getNber;
+    console.log(getNber);
+    
+    var getEmail = document.getElementById('getEmail').value;
+   // document.getElementById('getFname').innerText = getEmail;
+    console.log(getEmail);
+    
+    var getAddress = document.getElementById('getAddress').value;
+   // document.getElementById('getFname').innerText = getAddress;
+    console.log(getAddress);
+    
+    var getStreet = document.getElementById('getStreet').value;
+   // document.getElementById('getFname').innerText = getStreet;
+    console.log(getStreet);
+    
+    var getCity = document.getElementById('getCity').value;
+   // document.getElementById('getFname').innerText = getCity;
+    console.log(getCity);
+    
+    var getState = document.getElementById('getState').value;
+    //document.getElementById('getFname').innerText = getState;
+    console.log(getState);
+    
+    var getZip = document.getElementById('getZip').value;
+   // document.getElementById('getFname').innerText = getZip;
+    console.log(getZip);
+    
+    var getType = document.getElementById('getType').value;
+   // document.getElementById('getFname').innerText = getType;
+    console.log(getType);
+    
+    var getReview = document.getElementById('getReview').value;
+    //document.getElementById('getFname').innerText = getReview;
+    console.log(getReview);
+    
+    
+
+
+}
